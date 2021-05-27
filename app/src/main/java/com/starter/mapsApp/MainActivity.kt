10477c6ff7d6582f8eff6af87f5f2733400e0741
r_elbouhi <rami.elbouhi@gmail.com>
2021-05-27 18:24:30 +0200

    - Complete GMS part.

--- app/src/main/java/com/starter/mapsApp/MainActivity.kt
+++ app/src/main/java/com/starter/mapsApp/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.rami.maps
+package com.starter.mapsApp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
@@ -11,11 +11,11 @@ import com.google.android.gms.maps.model.MarkerOptions
 
 class MainActivity : AppCompatActivity(), OnMapReadyCallback {
 
-    private lateinit var mMap: GoogleMap
+    private var mMap: GoogleMap? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
@@ -24,9 +24,9 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
         mMap = googleMap
         // Add a marker in Sydney and move the camera
         val sydney = LatLng(-34.0, 151.0)
-        mMap.addMarker(MarkerOptions()
+        mMap?.addMarker(MarkerOptions()
                 .position(sydney)
                 .title("Marker in Sydney"))
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
+        mMap?.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
